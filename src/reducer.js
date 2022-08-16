a222d53539c9a23831b2cbe7e7bce56d2e54d426
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-08-16 10:21:10 -0400

    update reducer remove from basket

--- src/reducer.js
+++ src/reducer.js
@@ -13,11 +13,16 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item],
             }
         case 'REMOVE_FROM_BASKET':
-            let basketCopy = [...state.basket];
-            const filteredBasket = basketCopy.filter((item) => item.id !== action.id);
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            }
             return {
                 ...state,
-                basket: filteredBasket,
+                basket: newBasket,
             }
         case 'SET_USER':
             return {
